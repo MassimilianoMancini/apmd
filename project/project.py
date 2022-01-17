63acc2d9c566709ec8ebb87c9db50092e0066928
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2022-01-17 17:33:03 +0100

    fixed node load and output for long loading

--- project/project.py
+++ project/project.py
@@ -5,5 +5,7 @@ import networkx as nx
 G = nx.Graph()
 for filename in os.listdir("sequences"):
     with open(os.path.join("sequences", filename), 'r') as f:
-        G.add_node(json.load(f)['query'][-3:])
+        nodeName = json.load(f)['query'][3:]
+        G.add_node(nodeName)
+        print('Added node [%s%%]\r'%nodeName, end="")
 print(G)

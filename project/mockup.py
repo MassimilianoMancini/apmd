4bdd55302509a1554eff004550bc94bdb71b7f60
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2022-01-25 12:03:05 +0100

    mockup for loading done

--- project/mockup.py
+++ project/mockup.py
@@ -3,6 +3,10 @@ import re
 import os
 import itertools
 import networkx as nx
+import logging
+
+logging.basicConfig(filename='import.log', encoding='utf-8', level=logging.DEBUG)
+
 
 authors = {}
 i = 0.0
@@ -19,26 +23,27 @@ for filename in os.listdir('sequences'):
         if (j % 10 == 0):
             print (f"{j}%", end="")
     
-    f = open(os.path.join('sequences', filename), 'r')
-    data = json.load(f)
-    nodeName = data['query'][3:]
-    if 'results' in data and 'comment' in data['results'][0]:
-        authors = []
-        for s in data['results'][0]['comment']:
-            x = re.search(r"- _([a-zA-Z., -]+)_", s)
-            if x:
-                authors.append(x.group(1))
-            
-        if (len(authors) == 0):
-            print(f"file {nodeName} does not have authors")
+    with open(os.path.join('sequences', filename), 'r') as f:
+        data = json.load(f)
+        nodeName = data['query'][3:]
+        if 'results' in data and 'comment' in data['results'][0]:
+            authors = []
+            for s in data['results'][0]['comment']:
+                x = re.search(r"- _([a-zA-Z., -]+)_", s)
+                if x:
+                    authors.append(x.group(1))
+                
+            if (len(authors) < 2):
+                logging.info(f"file {nodeName} does not have enough authors")
+            else:
+                G.add_edges_from(itertools.combinations(authors, 2), label=nodeName)
+
         else:
-            G.add_edges_from(itertools.combinations(authors, 2), label=nodeName)
-
-    else:
-        print(f"file {nodeName} does not have comment section")
-    f.close()
-
-print("\nData import completed. Creating graph...")
-
+            logging.info(f"file {nodeName} does not have comment section")
+print ("\n")
 print(G)
+print ("\n")
+print(G.nodes)
+print ("\n")
+print(G.edges(data=True))
 

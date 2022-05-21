f2a938e35ea8f1309de0550ae15951f47b09a55b
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2022-05-21 15:45:41 +0200

    star reading file

--- project/mockup.py
+++ project/mockup.py
@@ -1,55 +1,10 @@
 import re
-import os
-import itertools
-import networkx as nx
 
-class ViewProgress:
-    def __init__(self, t):
-        self.t = t
-        self.i = 0.0
-        self.j = 1
-        print ("Import data from IMDB file")
-        print ("0%", end="")
+f = open("project/imdb-actors-actresses-movies.tsv", "r")
+lines = f.readlines()
+for line in lines:
+    actor, movie = line.split("\t")
+    print (actor, movie)
+    year = re.search("(\()(\d\d\d\d)([\/[IXV]*]*\))", movie).group(2)
+    print (year)
 
-    def update(self):
-        self.i = self.i + (100/self.t)
-        if (self.i > self.j and self.j <= 100):
-            print (".", end="")
-            self.j = self.j + 1
-            if (self.j % 10 == 0):
-                print (f"{self.j}%", end="")
-
-        if (self.i == self.t): 
-            print (f"{self.i} tsv file imported")
-        
-class IMDBFile:
-    with open(os.path.join(path, filename)) as f:
-        file = {}
-        file['id'] = jsonData['query'][3:]
-        if 'results' in jsonData and 'comment' in jsonData['results'][0]:
-            file['comment'] = jsonData['results'][0]['comment']
-            items.append(file)
-        vp.update()
-    return items
-
-class APMDGraph(nx.Graph):    
-    def createFromFiles(self, path):
-        authors = {}
-        i = 0.0
-        j = 1
-        oeisfiles = OEISJsonFiles()
-        files = oeisfiles.withCommentSection(path)
-        for file in files:            
-            authors = []
-            for s in file['comment']:
-                x = re.search(r"- _([a-zA-Z., -]+)_", s)
-                if x:
-                    authors.append(x.group(1))
-                        
-            if (len(authors) >= 2):
-                self.add_edges_from(itertools.combinations(authors, 2), label=file['id'])
-
-g = APMDGraph()
-g.createFromFiles('sequences')
-
-print(g)

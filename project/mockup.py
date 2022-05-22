c9d7a52e7ddf5b606c76e99be9a7a4c9b5e28c08
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2022-05-22 12:52:47 +0200

    graph creation

--- project/mockup.py
+++ project/mockup.py
@@ -1,10 +1,28 @@
 import re
+import networkx as nx
+G = nx.Graph()
+
+# RE
+# will work on movie part of string 
+# first group an opening parenthesis
+# second group the year (4 digits)
+# third optional group slash with roman numbers
+# four group a closing parenthesis
+p = re.compile("(\()(\d\d\d\d)([/IXV])*(\))")
 
 f = open("project/imdb-actors-actresses-movies.tsv", "r")
 lines = f.readlines()
 for line in lines:
     actor, movie = line.split("\t")
-    print (actor, movie)
-    year = re.search("(\()(\d\d\d\d)([\/[IXV]*]*\))", movie).group(2)
-    print (year)
 
+
+    y = p.search(movie)
+    #discard not matching string, e.g. lines without year see Aamundson
+    if y:
+        # get second group, 4 digits i.e. the year
+        year = y.group(2)
+        G.add_node(actor, type="actor")
+        G.add_node(movie, type="movie", year=year)
+        G.add_edge(actor, movie)
+
+print (G)

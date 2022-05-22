036a27545f61276eff5f9f870692b289838df182
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2022-05-22 18:09:02 +0200

    classes

--- project/mockup.py
+++ project/mockup.py
@@ -1,28 +1,63 @@
+import logging
 import re
 import networkx as nx
-G = nx.Graph()
 
-# RE
-# will work on movie part of string 
-# first group an opening parenthesis
-# second group the year (4 digits)
-# third optional group slash with roman numbers
-# four group a closing parenthesis
-p = re.compile("(\()(\d\d\d\d)([/IXV])*(\))")
+class MovieGraph():
+    def __init__(self, path):
+        self.graph = nx.Graph()
+        self.path = path
+        # Regular Expression
+        # will work on movie part of string 
+        # first group an opening parenthesis
+        # second group the year (4 digits)
+        # third optional group slash with roman numbers
+        # four group a closing parenthesis
+        self.reForYear = re.compile("(\()(\d\d\d\d)([/IXV])*(\))")
 
-f = open("project/imdb-actors-actresses-movies.tsv", "r")
-lines = f.readlines()
-for line in lines:
-    actor, movie = line.split("\t")
+    def logProblems(self):
+        log = open("project/MovieGraphImportLogger.log", "w")
+        G = nx.Graph()
+
+        f = open(self.path, "r")
+        lines = f.readlines()
+        for line in lines:
+            actor, movie = line.split("\t")
+            if G.has_edge(actor, movie):
+                log.write("DUPLICATE: " + actor +  ":" +  movie)
+            else:
+                G.add_edge(actor, movie)
+                # Check for yar problem only if edge does not exist
+                # This way number of lines of log file
+                # and nodes will be consistent
+                # |G| = lines of input file - errors
+                # 8.104.335 - 855 = 8.103.480
+                if not self.reForYear.search(movie):
+                    log.write("NO YEAR: " + actor +  ":" +  movie)
+        f.close
+        log.close
+
+    def createFromFile(self):
+        f = open(self.path, "r")
+        lines = f.readlines()
+        for line in lines:
+            actor, movie = line.split("\t")
+            y = self.reForYear.search(movie)
+            #discard not matching string, e.g. lines without year see Aamundson
+            if y:
+                # get second group, 4 digits i.e. the year
+                year = y.group(2)
+                self.graph.add_node(actor, type="actor")
+                self.graph.add_node(movie, type="movie", year=year)
+                self.graph.add_edge(actor, movie)
+        f.close
 
 
-    y = p.search(movie)
-    #discard not matching string, e.g. lines without year see Aamundson
-    if y:
-        # get second group, 4 digits i.e. the year
-        year = y.group(2)
-        G.add_node(actor, type="actor")
-        G.add_node(movie, type="movie", year=year)
-        G.add_edge(actor, movie)
-
-print (G)
+# START HERE
+path = "project/imdb-actors-actresses-movies.tsv"
+G = MovieGraph(path)
+# You could split into two sections: first find problems, second fast create consistent graph
+G.logProblems()
+print ("Problems found")
+# Create graph after problems were found
+G.createFromFile()
+print (G.graph)
